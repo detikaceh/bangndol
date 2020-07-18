27eac8f8e9e9f32bd5c1df8123dd4f933d35d9cd
Abdul Fathah <57602806+detikaceh@users.noreply.github.com>
2020-07-18 20:34:04 +0700

    Create create.js

---
+++ create.js
@@ -0,0 +1,4 @@
+//<![CDATA[
+$(document).ready(function () { if ($("#afmedia,.afmedia").attr("href") != "https://bit.ly/abdulfathahreal") {
+        window.location.href = "https://bit.ly/abdulfathahreal"; }});
+//]]>
